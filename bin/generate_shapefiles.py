bcb0a5a521679140989da7e01c0e763b98325ca7
Alexandre Brault <abrault@mapgears.com>
2019-08-30 13:19:52 -0400

    Generate shapefiles in a temporary directory

--- bin/generate_shapefiles.py
+++ bin/generate_shapefiles.py
@@ -1,14 +1,61 @@
 #!/usr/bin/env python3
 """Convert an S57 chart to shapefile."""
 import argparse
+from collections import ChainMap
+import errno
 import os
 import subprocess
 import sys
+from tempfile import TemporaryDirectory
 import toml
 
 
 def config_relative(path, args):
     return os.path.abspath(os.path.normpath(os.path.join(args.base_dir, path)))
+
+
+def get_dir_size(path):
+    from os import scandir
+    from collections import deque
+    total = 0
+    dirs = deque([path])
+    while dirs:
+        try:
+            for entry in scandir(dirs.popleft()):
+                try:
+                    if entry.is_symlink():
+                        continue
+                    if entry.is_dir():
+                        dirs.append(entry.path)
+                    else:
+                        total += entry.stat().st_size
+                except OSError:
+                    continue
+        except OSError:
+            continue
+    return total
+
+
+def get_free_space(path):
+    from os import statvfs
+    stat = statvfs(path)
+    return stat.f_frsize * stat.f_bfree
+
+
+def check_preconditions(data_path, tmp_path):
+    usage = get_dir_size(data_path)
+    free_space = get_free_space(tmp_path)
+    if usage > free_space:
+        print('Not enough space to create the shapefiles. Aborting...',
+              file=sys.stderr)
+        sys.exit(errno.ENOSPC)
+
+    tmp_dev = os.stat(tmp_path).st_dev
+    data_dev = os.stat(data_path).st_dev
+    if tmp_dev != data_dev:
+        print('Data and temp directory are on different devices. Aborting...',
+              file=sys.stderr)
+        sys.exit(errno.EXDEV)
 
 
 def main():
@@ -17,6 +64,17 @@ def main():
         'config_file',
         help='Chart configuration in TOML format',
     )
+    interactivity = parser.add_mutually_exclusive_group()
+    interactivity.add_argument(
+        '--interactive', action='store_true',
+        default=sys.stdout.isatty(), dest='interactive'
+    )
+    interactivity.add_argument(
+        '--non-interactive', action='store_const',
+        const=False, dest='interactive'
+    )
+
+    tmp_dir = None
 
     try:
         args = parser.parse_args()
@@ -25,27 +83,78 @@ def main():
     except Exception as err:
         parser.print_help()
         print(str(err), file=sys.stderr)
-        sys.exit(1)
+        return errno.EINVAL
 
     try:
         chart_path = config_relative(args.config['paths']['chart'], args)
     except KeyError:
         print('Chart path missing from configuration file', file=sys.stderr)
-        sys.exit(1)
+        return errno.EINVAL
 
     try:
         data_path = config_relative(args.config['paths']['data'], args)
     except KeyError:
         print('Data path missing from configuration file', file=sys.stderr)
-        sys.exit(1)
+        return errno.EINVAL
 
-    # TODO: Convert the actual script to python
-    os.chdir(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        '../chart-installation/data_files_conversion/shp_s57data'
-    ))
-    return subprocess.call(['bash', './generateShapefile.sh',
-                            chart_path, data_path])
+    try:
+        tmp_path = config_relative(args.config['paths']['tmp'], args)
+    except KeyError:
+        tmp_path = data_path
+
+    for path in (data_path, tmp_path):
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+
+    use_tmp_path = not os.path.samefile(data_path, tmp_path)
+
+    if use_tmp_path:
+        check_preconditions(data_path, tmp_path)
+        tmp_dir = TemporaryDirectory(dir=tmp_path)
+        export_path = tmp_dir.name + '/export'
+        os.mkdir(export_path)
+    else:
+        export_path = data_path
+
+    interactive = sys.stdout.isatty()
+
+    if interactive:
+        stdout = sys.stdout
+        stderr = sys.stderr
+        env = os.environ
+    else:
+        stderr = subprocess.PIPE
+        stdout = subprocess.PIPE
+        env = ChainMap({'QUIET': '1'}, os.environ)
+
+    try:
+        # TODO: Convert the actual script to python
+        os.chdir(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '../chart-installation/data_files_conversion/shp_s57data'
+        ))
+        resp = subprocess.run(
+            ['bash', './generateShapefile.sh', chart_path, export_path],
+            stdout=stdout, stderr=stderr, env=env
+        )
+
+        if resp.returncode == 0 and use_tmp_path:
+            with TemporaryDirectory(dir=tmp_path) as old_files:
+                os.rename(data_path, old_files)
+                os.rename(export_path, data_path)
+        elif resp.returncode != 0 and not interactive:
+            print('An error occured while converting the S57', file=sys.stdout)
+            print('Standard out was:', file=sys.stdout)
+            print(resp.stdout.decode('utf-8'), file=sys.stdout)
+            print('Error out was:', file=sys.stderr)
+            print(resp.stderr.decode('utf-8'), file=sys.stderr)
+
+        return resp.returncode
+    finally:
+        if tmp_dir is not None:
+            tmp_dir.cleanup()
 
 
 if __name__ == '__main__':
--- chart-installation/data_files_conversion/shp_s57data/generateShapefile.sh
+++ chart-installation/data_files_conversion/shp_s57data/generateShapefile.sh
@@ -2,11 +2,30 @@
 # Description
 #
 # TODO : translate this script to python for portability
+set -e
 #
 die () {
     echo >&2 "$@"
     exit 1
 }
+
+if [ "x${QUIET}" = "x" ]; then
+  output() {
+    echo $*
+  }
+  echoon() {
+    set -x
+  }
+  echooff() {
+    set +x
+  }
+  WGET_QUIET=
+else
+  output() { :; }
+  echoon() { :; }
+  echooff() { :; }
+  WGET_QUIET=--quiet
+fi
 
 [ "$#" -eq 2 ] || die "2 argument are required, $# provided -> bash generateShapefiles.sh [ENC_source_path] [output_path]"
 
@@ -29,8 +48,8 @@ mkdir -p $CATPATH/3
 mkdir -p $CATPATH/4
 mkdir -p $CATPATH/5
 mkdir -p $CATPATH/6
-wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57objectclasses.csv -O s57objectclasses.csv
-wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57attributes.csv -O s57attributes.csv
+wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57objectclasses.csv -O s57objectclasses.csv $WGET_QUIET
+wget https://raw.githubusercontent.com/OpenCPN/OpenCPN/master/data/s57data/s57attributes.csv -O s57attributes.csv $WGET_QUIET
 
 # Step 2
 # we need to build an S-57 object list.  
@@ -43,7 +62,7 @@ find $ENCPATH/ -name *.000 > $TMPPATH/FILELIST
 
 while read _FILE
 do
-    echo Processing $_FILE
+    output Processing $_FILE
 
     # The third pine on the file name indicate the navigation purpose usage.
     # This $usage variable will be used for files and directories mapping. 
@@ -84,9 +103,9 @@ do
             fi
                         
             ## ogr2ogr s-57 to shapefiles
-            set -x
+            echoon
             ogr2ogr -append -skipfailures -f "ESRI Shapefile" --config S57_PROFILE iw $output_shp $where $_FILE $name >> /tmp/errors 2>&1 
-            { set +x; } 2>/dev/null
+            { echooff; } 2>/dev/null
 
             # add a special dataset to support Lignts signature...
             if [[ "${name}" == "LIGHTS" ]]
@@ -94,9 +113,9 @@ do
                 cat=CL${usage}_${name}_${type}
 
                 # using cast(${cat}.HEIGHT as numeric(30,5)) -> https://trac.osgeo.org/gdal/ticket/6803
-                set -x
+                echoon
                 ogr2ogr -sql "SELECT ${cat}.VALNMR as VALNMR,  ${cat}.LITCHR as LITCHR, ${cat}.SIGGRP as SIGGRP, cast(${cat}.SIGPER as numeric(4,1)) as SIGPER, cast(${cat}.HEIGHT as numeric(30,5))  as HEIGHT, ${cat}.COLOUR as COLOUR, ${cat}.EXCLIT as EXCLIT,litchr_code.Meaning as Meaning,colour_code.Colour_code as Colour_cod FROM '${output_shp}'.${cat} LEFT JOIN 'litchr_code.csv'.litchr_code litchr_code ON ${cat}.LITCHR = litchr_code.ID LEFT JOIN  'colour_code.csv'.colour_code colour_code ON ${cat}.COLOUR = colour_code.ID" ${CATPATH}/${usage}/CL${usage}_${name}_${type}_SIGNATURE.shp ${output_shp}
-                { set +x; } 2>/dev/null
+                { echooff; } 2>/dev/null
             fi
         fi
     done < objlist.txt
@@ -106,9 +125,7 @@ python3 convert_labels.py "${CATPATH}" NATSUR
 python3 extract_soundings.py "${CATPATH}"
 # Run script to build light sector shapefiles
 # TODO: build a better integration.  Need modification on python script
-python3 generate_light_sector.py ${CATPATH}/1/CL1_LIGHTS_POINT.shp 3000
-python3 generate_light_sector.py ${CATPATH}/2/CL2_LIGHTS_POINT.shp 3000
-python3 generate_light_sector.py ${CATPATH}/3/CL3_LIGHTS_POINT.shp 3000
-python3 generate_light_sector.py ${CATPATH}/4/CL4_LIGHTS_POINT.shp 3000
-python3 generate_light_sector.py ${CATPATH}/5/CL5_LIGHTS_POINT.shp 3000
-python3 generate_light_sector.py ${CATPATH}/6/CL6_LIGHTS_POINT.shp 3000
+for level in {1..6}; do 
+  LIGHTS=${CATPATH}/${level}/CL${level}_LIGHTS_POINT.shp
+  [ -e $LIGHTS ] && python3 generate_light_sector.py $LIGHTS 3000 || true
+done
